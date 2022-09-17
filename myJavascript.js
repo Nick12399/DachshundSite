d950867435b7c8c532e78dcec7000ea8db2342fc
Nick12399 <81006391+Nick12399@users.noreply.github.com>
2022-09-17 17:07:01 -0500

    Add files via upload

---
+++ myJavascript.js
@@ -0,0 +1,15 @@
+function mOver(obj) {
+  obj.innerHTML = "Thank You"
+}
+
+function mOut(obj) {
+  obj.innerHTML = "Mouse over me"
+}
+
+function mOver2(obj){
+  obj.innerHTML = "<img src='./images/Thorandsophie.jpg' class='secretPic'><p class='secretCap'>My girlfriend and my dog Thor</p>"
+}
+
+function mOut2(obj) {
+  obj.innerHTML = "Mouse over me for a secret pic. :)"
+}
